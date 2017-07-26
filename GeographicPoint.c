ce82925228fe2cbfdecd2592bf0686b6ef2f155c
aesilva10 <ae.silva10@uniandes.edu.co>
2017-07-26 18:13:00 -0500

    Update GeographicPoint.c

--- GeographicPoint.c
+++ GeographicPoint.c
@@ -1,27 +1,80 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-
-
-
-int main(void){
+#include <time.h> 
+//numero de filas ycolumnas
+int n_col=744;
+int n_fil=500;
+//funciones
+int numerosx(int n_col);
+int numerosy(int n_fil);
+int R(int **mat);
+//funcion principal
+void main()
+{
+	//contadores
+  	int i;
+  	int j;
+	//matriz de datos 
+	int **mat=malloc(n_col*sizeof(int*)); 
+	for(i=0;i<n_col;i++)
+	{
+		mat[i]=malloc(n_fil*sizeof(int));
+	}
 	FILE *file;
-	file=fopen("map_data.txt","r");
-	int len=500;
-	char line_buffer[len];
-	char *split_buffer;
-	int i=0;
-	int j=0;
-	double *data= malloc(n*sizeof(double));		
-	while(fgets(line_buffer,len,file)){
-		j=0;
-		split_buffer=strtok(line_buffer);
-		while(split_buffer !=NULL){
-			if(j!=0){
-				matriz[i][j-1]=atof(split_buffer);}
-			split_buffer=strtok(NULL);
-			j+=1;
+	// abrir el archivo
+  	file=fopen("map_data.txt","r");
+	for(i=0; i<n_col;i++)
+	{
+		for(j=0;j<n_fil;j++) 
+		{
+		//Llenar la matriz con los datos
+		fscanf(file,"%d",&mat[i][j]);       		
+		}  		
+	}
+	fclose(file);
+	// semilla diferente dependiendo del tiempo hh:mm:ss del pc
+	srand(time(NULL));
+	// llamar las funciones
+	numerosx(n_col);
+	numerosy(n_fil);
+	R(mat);
+
+}
+// declaracion numeros aleatorios x
+int numerosx(int n_col)
+{	
+	int numero=rand()%(n_col+1);
+	return numero;	
+}
+// declaracion numeros aleatorios en y
+int numerosy(int n_fil)
+{	
+	int numero=rand()%(n_fil+1);
+	return numero;
+}	
+// declaracion funcion que calcula el r maximo
+int R(int **mat)
+{	
+	int rmax= (n_fil/2);
+	for(int r=1; r<rmax;r++)
+	{
+		int m= pow(2,0.5)*r/2;
+		for(int i=0;i<=m;i++)
+		{
+			for(int j=0; j<=m;j++)
+			{
+				if(mat[numerosx(n_col)+i+1][j]!=1 || mat[numerosx(n_col)+i+1][j]==NULL && mat[numerosx(n_col)-i-1][j]!=1 || mat[numerosx(n_col)-i-1][j]==NULL && mat[i][numerosy(n_fil)+j+1]!=1 || mat[i][numerosy(n_fil)+j+1]==NULL && mat[i][numerosy(n_fil)-j-1]!=1 || mat[i][numerosy(n_fil)-j-1]==NULL && mat[numerosx(n_col)+i][numerosy(n_fil)+j]!=1 && mat[numerosx(n_col)-i][numerosy(n_fil)-j]!=1 && mat[numerosx(n_col)+i][numerosy(n_fil)-j]!=1 && mat[numerosx(n_col)-i][numerosy(n_fil)+j]!=1)
+					{
+					continue;
+					}
+				else
+					{
+					return r;
+					printf("%d\n",r);
+					}
+			}		
 		}
-	i+=1;
 	}
 }
+// Segmentation fault (core dumped) :(
