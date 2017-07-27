d438b380dd9a91a068e50c616a7d1decd7976084
aesilva10 <ae.silva10@uniandes.edu.co>
2017-07-27 16:04:15 -0500

    Update GeographicPoint.c

--- GeographicPoint.c
+++ GeographicPoint.c
@@ -8,7 +8,10 @@ int n_fil=500;
 //funciones
 int numerosx(int n_col);
 int numerosy(int n_fil);
-int R(int **mat);
+int R(int **mat, int x, int y);
+double montecarlo(int **mat);
+int inx(int x);
+int iny(int x);
 //funcion principal
 void main()
 {
@@ -38,43 +41,123 @@ void main()
 	// llamar las funciones
 	numerosx(n_col);
 	numerosy(n_fil);
-	R(mat);
+	int x,y;	
+	inx(x);
+	iny(x);	
+	R(mat,x,y);
+	montecarlo(mat);
 
 }
-// declaracion numeros aleatorios x
+//numeros aleatorios x
 int numerosx(int n_col)
 {	
-	int numero=rand()%(n_col+1);
+	int numero=rand()%(n_col);
 	return numero;	
 }
-// declaracion numeros aleatorios en y
+//numeros aleatorios en y
 int numerosy(int n_fil)
 {	
-	int numero=rand()%(n_fil+1);
+	int numero=rand()%(n_fil);
 	return numero;
 }	
-// declaracion funcion que calcula el r maximo
-int R(int **mat)
+
+// funcion que calcula el r maximo
+int R(int **mat, int x, int y)
 {	
-	int rmax= (n_fil/2);
-	for(int r=1; r<rmax;r++)
+	int *rad=malloc(744*sizeof(int));
+	int rmax= n_fil/2;
+	for(int r=2; r<rmax;r++)
 	{
-		int m= pow(2,0.5)*r/2;
-		for(int i=0;i<=m;i++)
+		int m=r/pow(2,0.5);
+		for(int i=0;i<m;i++)
 		{
-			for(int j=0; j<=m;j++)
+			for(int j=0; j<m;j++)
 			{
-				if(mat[numerosx(n_col)+i+1][j]!=1 || mat[numerosx(n_col)+i+1][j]==NULL && mat[numerosx(n_col)-i-1][j]!=1 || mat[numerosx(n_col)-i-1][j]==NULL && mat[i][numerosy(n_fil)+j+1]!=1 || mat[i][numerosy(n_fil)+j+1]==NULL && mat[i][numerosy(n_fil)-j-1]!=1 || mat[i][numerosy(n_fil)-j-1]==NULL && mat[numerosx(n_col)+i][numerosy(n_fil)+j]!=1 && mat[numerosx(n_col)-i][numerosy(n_fil)-j]!=1 && mat[numerosx(n_col)+i][numerosy(n_fil)-j]!=1 && mat[numerosx(n_col)-i][numerosy(n_fil)+j]!=1)
-					{
-					continue;
+				if(mat[inx(x+i)][iny(j)]!=1 && mat[inx(x-i)][iny(j)]!=1 && mat[inx(i)][iny(y+j)]!=1 && mat[inx(i)][iny(y-j)]!=1 && mat[inx(x+i)][iny(y+j)]!=1 && mat[inx(x-i)][iny(y-j)]!=1 && mat[inx(x+i)][iny(y-j)]!=1 && mat[inx(x-i)][iny(y+j)]!=1 && mat[inx(x)][iny(y)]!=1)
+					{								
+					continue;										
 					}
-				else
+					else
 					{
-					return r;
-					printf("%d\n",r);
+					return r;					
+					break;
 					}
-			}		
+			}
 		}
 	}
 }
-// Segmentation fault (core dumped) :(
+int inx(int x)
+{	
+	if(x<0)
+	{
+		return x+n_col;
+	}	
+	if(x>n_col)
+	{
+		return x-n_col;
+	}
+	if(x<=n_col && x>=0)
+	{
+		return x;
+	}
+}
+int iny(int x)
+{	
+	if(x<0)
+	{
+		return x+n_fil;
+	}	
+	if(x>n_fil)
+	{
+		return x-n_fil;
+	}
+	if(x<=n_fil && x>=0)
+	{
+		return x;
+	}
+}
+double montecarlo(int **mat)
+{
+	double x_new, x_last, y_new, y_last,r_new, r_last, alpha;
+	double delta=0.5;
+	x_last=numerosx(n_col)-1;
+	y_last=numerosy(n_col)-1;
+	r_last=R(mat,x_last,y_last);
+	for(int i=0;i<n_col;i++)
+	{
+	x_new=x_last + (numerosx(n_col)-1)*delta;
+	y_new=y_last + (numerosy(n_fil)-1)*delta;
+	r_new=R(mat,x_new,y_new);
+	alpha=r_new/r_last;
+		if(alpha>1)
+		{
+			alpha=1;	
+		}
+		if(alpha>numerosx(1))
+		{
+			x_new=x_last;
+			y_new=y_last;
+			r_new=r_last;	
+		}
+	printtf("%f %f %f\n", x_last,y_last,r_last);
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+	
+
+
+
+
+
+
